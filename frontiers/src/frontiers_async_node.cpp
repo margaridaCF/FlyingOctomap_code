71a4f3ca417fbda30ecc36dc9a3064d8f99702fd
Unknown <margaridaCostaFaria@gmail.com>
2018-03-12 18:01:20 +0100

    [frontiers] Taking into account octomap might not have arrived yet.

--- frontiers/src/frontiers_async_node.cpp
+++ frontiers/src/frontiers_async_node.cpp
@@ -4,29 +4,42 @@
 #include <octomap_msgs/Octomap.h>
 #include <octomap_msgs/conversions.h>
 
-
-octomap::OcTree* octree;
-ros::Publisher local_pos_pub;
-
-void frontier_callback(const frontiers_msgs::FrontierRequest::ConstPtr& frontier_request)
+namespace frontiers_async_node
 {
-	frontiers_msgs::FrontierReply reply;
-	bool outcome = Frontiers::processFrontiersRequest(*octree, *frontier_request, reply);
-	local_pos_pub.publish(reply);
-}
+	octomap::OcTree* octree;
+	ros::Publisher local_pos_pub;
+	bool octomap_init;
 
-void octomap_callback(const octomap_msgs::Octomap::ConstPtr& octomapBinary){
-	delete octree;
-	octree = (octomap::OcTree*)octomap_msgs::binaryMsgToMap(*octomapBinary);
+	void frontier_callback(const frontiers_msgs::FrontierRequest::ConstPtr& frontier_request)
+	{
+		frontiers_msgs::FrontierReply reply;
+		if(octomap_init)
+		{
+			Frontiers::processFrontiersRequest(*octree, *frontier_request, reply);
+		}
+		else
+		{
+			reply.success=false;
+			reply.request_id = frontier_request->header.seq;
+			reply.frontiers_found = 0;
+		}
+		local_pos_pub.publish(reply);
+	}
+
+	void octomap_callback(const octomap_msgs::Octomap::ConstPtr& octomapBinary){
+		delete octree;
+		octree = (octomap::OcTree*)octomap_msgs::binaryMsgToMap(*octomapBinary);
+		octomap_init = true;
+	}
 }
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "frontier_node_async");
 	ros::NodeHandle nh;
-	ros::Subscriber octomap_sub = nh.subscribe<octomap_msgs::Octomap>("/octomap_binary", 10, octomap_callback);
-	ros::Subscriber frontiers_sub = nh.subscribe<frontiers_msgs::FrontierRequest>("frontiers_request", 10, frontier_callback);
-	local_pos_pub = nh.advertise<frontiers_msgs::FrontierReply>("frontiers_reply", 10);
+	ros::Subscriber octomap_sub = nh.subscribe<octomap_msgs::Octomap>("/octomap_binary", 10, frontiers_async_node::octomap_callback);
+	ros::Subscriber frontiers_sub = nh.subscribe<frontiers_msgs::FrontierRequest>("frontiers_request", 10, frontiers_async_node::frontier_callback);
+	frontiers_async_node::local_pos_pub = nh.advertise<frontiers_msgs::FrontierReply>("frontiers_reply", 10);
 
 	ros::spin();
 }
