61dd4af76e4eb00e9f074964c07c7fd5a13809b3
Unknown <margaridaCostaFaria@gmail.com>
2018-03-13 18:36:15 +0100

    [frontiers] Creating service call to check if frontiers node has received any octomap yet.

--- frontiers/src/frontiers_async_node.cpp
+++ frontiers/src/frontiers_async_node.cpp
@@ -3,12 +3,20 @@
 #include <octomap/OcTree.h>
 #include <octomap_msgs/Octomap.h>
 #include <octomap_msgs/conversions.h>
+#include <frontiers_msgs/FrontierNodeStatus.h>
 
 namespace frontiers_async_node
 {
 	octomap::OcTree* octree;
 	ros::Publisher local_pos_pub;
 	bool octomap_init;
+
+	bool check_status(frontiers_msgs::FrontierNodeStatus::Request  &req,
+        frontiers_msgs::FrontierNodeStatus::Response &res)
+	{
+		res.is_accepting_requests = octomap_init;
+	  	return true;
+	}
 
 	void frontier_callback(const frontiers_msgs::FrontierRequest::ConstPtr& frontier_request)
 	{
@@ -37,6 +45,7 @@ int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "frontier_node_async");
 	ros::NodeHandle nh;
+	ros::ServiceServer service = nh.advertiseService("frontier_status", frontiers_async_node::check_status);
 	ros::Subscriber octomap_sub = nh.subscribe<octomap_msgs::Octomap>("/octomap_binary", 10, frontiers_async_node::octomap_callback);
 	ros::Subscriber frontiers_sub = nh.subscribe<frontiers_msgs::FrontierRequest>("frontiers_request", 10, frontiers_async_node::frontier_callback);
 	frontiers_async_node::local_pos_pub = nh.advertise<frontiers_msgs::FrontierReply>("frontiers_reply", 10);
