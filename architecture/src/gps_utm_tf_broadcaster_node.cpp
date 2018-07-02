dae8a6cc427a968bc4ec34b944953c3b27d00e27
Unknown <margaridaCostaFaria@gmail.com>
2018-07-02 09:52:45 +0200

    [architecture] tf broadcaster world to uav builds.

--- architecture/src/gps_utm_tf_broadcaster_node.cpp
+++ architecture/src/gps_utm_tf_broadcaster_node.cpp
@@ -1,18 +1,21 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
 #include <nav_msgs/Odometry.h>
 
 
 namespace gps_utm_tf_broadcaster
 {
   std::string uav_f;
-  std:string world_f;
+  std::string world_f;
 
   void poseCallback(const nav_msgs::OdometryConstPtr& msg){
     static tf::TransformBroadcaster br;
     tf::Transform transform;
     transform.setOrigin( tf::Vector3(msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z) );
-    transform.setRotation(msg->pose.pose.orientation);
+    tf::Quaternion q;
+    tf::quaternionMsgToTF(msg->pose.pose.orientation, q);
+    transform.setRotation(q);
     br.sendTransform(tf::StampedTransform(transform, msg->header.stamp, world_f, uav_f));
   }
   
@@ -24,10 +27,10 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "gps_utm_tf_broadcaster");
   ros::NodeHandle node;
 
-  nh.getParam("uav_frame", gps_utm_tf_broadcaster::uav_f);
-  nh.getParam("world_frame", gps_utm_tf_broadcaster::world_f);
+  node.getParam("uav_frame", gps_utm_tf_broadcaster::uav_f);
+  node.getParam("world_frame", gps_utm_tf_broadcaster::world_f);
 
-  ros::Subscriber sub = node.subscribe(+"mavros/global_position/local", 10, &poseCallback);
+  ros::Subscriber sub = node.subscribe(+"mavros/global_position/local", 10, &gps_utm_tf_broadcaster::poseCallback);
 
   ros::spin();
   return 0;
