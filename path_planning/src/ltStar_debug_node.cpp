71d3821520beef4de0f31b07ef949a111de30e7a
Unknown <margaridaCostaFaria@gmail.com>
2018-08-20 00:27:51 +0200

    [ltstar] Roll + yaw

--- path_planning/src/ltStar_debug_node.cpp
+++ path_planning/src/ltStar_debug_node.cpp
@@ -179,9 +179,7 @@ namespace LazyThetaStarOctree
 		marker_pub.publish(marker_array_original);
 
 		// CALCULATE ROTATION 
-    	// tf2::Vector3 zAxis (0, 0, 1);
     	tf2::Vector3 xAxis (1, 0, 0);
-    	// tf2::Transform rotation_yaw = generateRotation(zAxis, -(M_PI/4));
     	tf2::Transform rotation_roll = generateRotation(xAxis, (M_PI/4));
 
 		// Offset calculation
@@ -191,6 +189,69 @@ namespace LazyThetaStarOctree
 
 		tf2::Transform final_transform_start = rotation_roll;
 		tf2::Transform final_transform_end   = translation_from_center * rotation_roll * translation_to_center__;
+
+		//     APPLY ROTATION 
+		tf2::Vector3 rotated_start, rotated_end;
+		/*for (int i = 0; i < point_count; ++i)
+		{
+			rotated_start = final_transform_start * start_points[i];
+			rotated_end   = final_transform_end   * end_points[i];
+			rviz_interface::push_arrow_corridor(
+				octomath::Vector3 (rotated_start.getX(), rotated_start.getY(), rotated_start.getZ()), 
+				octomath::Vector3 (rotated_end.getX(), rotated_end.getY(), rotated_end.getZ()), 
+				marker_pub, 40+i, marker_array_rotated);
+		}
+		marker_pub.publish(marker_array_rotated);*/
+	}
+
+	void learnTf_theHardWay_pointSet_yaw_roll()
+	{
+		// PUBLISH ORIGINAL
+		const int point_count = 5;
+		std::array < tf2::Vector3, point_count> start_points =
+			{
+				tf2::Vector3(0,  2, 0),
+				tf2::Vector3(0,  1, 0),
+				tf2::Vector3(0,  0, 0),
+				tf2::Vector3(0, -1, 0),
+				tf2::Vector3(0, -2, 0),
+			};
+		std::array < tf2::Vector3, point_count> end_points =
+			{
+				tf2::Vector3(5, -3, 5),
+				tf2::Vector3(5, -4, 5),
+				tf2::Vector3(5, -5, 5),
+				tf2::Vector3(5, -6, 5),
+				tf2::Vector3(5, -7, 5),
+			};
+
+		visualization_msgs::MarkerArray  marker_array_original, marker_array_rotated;
+		/*for (int i = 0; i < point_count; ++i)
+		{
+			rviz_interface::push_arrow_corridor(
+				octomath::Vector3 (start_points[i].getX(), start_points[i].getY(), start_points[i].getZ()), 
+				octomath::Vector3 (end_points[i].getX(), end_points[i].getY(), end_points[i].getZ()), 
+				marker_pub, 30+i, marker_array_original);
+		}*/
+		marker_pub.publish(marker_array_original);
+
+		// CALCULATE ROTATION 
+    	tf2::Vector3 zAxis (0, 0, 1);
+    	tf2::Vector3 xAxis (1, 0, 0);
+    	tf2::Transform rotation_yaw = generateRotation(zAxis, -(M_PI/4));
+    	tf2::Transform rotation_roll = generateRotation(xAxis, (M_PI/4));
+
+		// Offset calculation START
+		tf2::Transform translation_to_center_start;
+		tf2::Transform translation_from_center_start;
+		generateTranslations(start_points[2], translation_to_center_start, translation_from_center_start);
+		// Offset calculation END
+		tf2::Transform translation_to_center_end;
+		tf2::Transform translation_from_center_end;
+		generateTranslations(end_points[2], translation_to_center_end, translation_from_center_end);
+
+		tf2::Transform final_transform_start = translation_from_center_start * rotation_roll * rotation_yaw * translation_to_center_start;
+		tf2::Transform final_transform_end   = translation_from_center_end * rotation_roll * rotation_yaw * translation_to_center_end;
 
 		//     APPLY ROTATION 
 		tf2::Vector3 rotated_start, rotated_end;
@@ -205,134 +266,6 @@ namespace LazyThetaStarOctree
 		}
 		marker_pub.publish(marker_array_rotated);
 	}
-
-
-	void learnTf_builtInFunctions()
-	{
-      	tf2::Vector3 toTest_start (0, 0, 0);
-		tf2::Vector3 toTest_end (1, 0, 0);
-    	tf2::Vector3 origin (0, 0, 0);
-    	tf2::Quaternion ypr_yaw_pitch ;
-    	ypr_yaw_pitch.setEuler(M_PI/2, 0, M_PI/2) ;	
-		tf2::Transform rotation_yaw_pitch;
-
-		rotation_yaw_pitch.setOrigin(origin);
-		rotation_yaw_pitch.setRotation(ypr_yaw_pitch);
-      	
-		rviz_interface::publish_arrow_path_unreachable(
-			octomath::Vector3 (toTest_start.getX(), toTest_start.getY(), toTest_start.getZ()), 
-			octomath::Vector3 (toTest_end.getX(), toTest_end.getY(), toTest_end.getZ()), 
-			marker_pub, 10);	
-
-		toTest_start = rotation_yaw_pitch * toTest_start;
-		toTest_end = rotation_yaw_pitch * toTest_end;
-
-		rviz_interface::publish_arrow_path_unreachable(
-			octomath::Vector3 (toTest_start.getX(), toTest_start.getY(), toTest_start.getZ()), 
-			octomath::Vector3 (toTest_end.getX(), toTest_end.getY(), toTest_end.getZ()), 
-			marker_pub, 2);	
-	}
-
-	void learnTf_builtInFunctions_twoSteps()
-	{
-      	tf2::Vector3 toTest_start (0, 0, 0);
-		tf2::Vector3 toTest_end (1, 0, 0);
-    	tf2::Vector3 origin (0, 0, 0);
-    	tf2::Quaternion ypr_yaw, ypr_pitch ;
-    	ypr_yaw.setEuler(0, 0, M_PI/2 ) ;	
-    	ypr_pitch.setEuler(M_PI/2, 0, 0) ;	
-		tf2::Transform rotation_yaw, rotation_pitch;
-
-		rotation_yaw.setOrigin(origin);
-		rotation_yaw.setRotation(ypr_yaw);
-
-		rotation_pitch.setOrigin(origin);
-		rotation_pitch.setRotation(ypr_pitch);
-      	
-		rviz_interface::publish_arrow_path_unreachable(
-			octomath::Vector3 (toTest_start.getX(), toTest_start.getY(), toTest_start.getZ()), 
-			octomath::Vector3 (toTest_end.getX(), toTest_end.getY(), toTest_end.getZ()), 
-			marker_pub, 10);	
-
-		toTest_start = rotation_yaw * toTest_start;
-		toTest_end = rotation_yaw * toTest_end;
-
-		toTest_start = rotation_pitch * toTest_start;
-		toTest_end = rotation_pitch * toTest_end;
-
-
-		rviz_interface::publish_arrow_path_unreachable(
-			octomath::Vector3 (toTest_start.getX(), toTest_start.getY(), toTest_start.getZ()), 
-			octomath::Vector3 (toTest_end.getX(), toTest_end.getY(), toTest_end.getZ()), 
-			marker_pub, 2);	
-	}
-
-	octomath::Vector3 getOrthogonalInXNormalized(octomath::Vector3 const& start, octomath::Vector3 const& goal)
-	{
-		octomath::Vector3 xAxis (1, 0, 0);
-		octomath::Vector3 sg = goal - start;
-		ROS_WARN_STREAM("sg = " << start << " - " << goal << " = " << sg); 
-		octomath::Vector3 orthogonal = sg.cross(xAxis);
-		orthogonal.normalize();
-		ROS_WARN_STREAM("orthogonal       = " << orthogonal);
-		orthogonal += start;
-		ROS_WARN_STREAM("Applied to start = " << orthogonal);
-
-		return orthogonal;
-	}
-
-	void rotateByCrossProduct()
-	{
-		// octomath::Vector3 toTest_start (0, 0, 0);
-		// octomath::Vector3 toTest_end (0, 0, 1);
-		// octomath::Vector3 orthogonal = getOrthogonalInXNormalized(toTest_start, toTest_end);
-		// octomath::Vector3 orthogonal_rightAnswer (0, -1, 0);
-		// ASSERT_EQ(orthogonal_rightAnswer, orthogonal);
-
-
-		// octomath::Vector3 toTest_start (0, 0, 0);
-		// octomath::Vector3 toTest_end (0, 0, 10);
-		// octomath::Vector3 orthogonal = getOrthogonalInXNormalized(toTest_start, toTest_end);
-		// octomath::Vector3 orthogonal_rightAnswer (0, -1, 0);
-		// ASSERT_EQ(orthogonal_rightAnswer, orthogonal);
-
-
-		// octomath::Vector3 toTest_start (0, 0, 0);
-		// octomath::Vector3 toTest_end (0, 1, 3);
-		// octomath::Vector3 orthogonal = getOrthogonalInXNormalized(toTest_start, toTest_end);
-		// octomath::Vector3 orthogonal_rightAnswer (0 -0.948683 0.316228);
-		// ASSERT_EQ(orthogonal_rightAnswer, orthogonal);
-
-		// octomath::Vector3 toTest_start (2, 2, 2);
-		// octomath::Vector3 toTest_end (2, 2, 3);
-		// octomath::Vector3 orthogonal = getOrthogonalInXNormalized(toTest_start, toTest_end);
-		// octomath::Vector3 orthogonal_rightAnswer (2 3 2);
-		// ASSERT_EQ(orthogonal_rightAnswer, orthogonal);
-
-
-		octomath::Vector3 toTest_start (0, 0, 0);
-		octomath::Vector3 toTest_end (0, 2, 0);
-		octomath::Vector3 orthogonal = getOrthogonalInXNormalized(toTest_start, toTest_end);
-		// octomath::Vector3 orthogonal_rightAnswer (2 3 2);
-		// ASSERT_EQ(orthogonal_rightAnswer, orthogonal);
-		
-
-		rviz_interface::publish_arrow_corridor(toTest_start, toTest_end, marker_pub, 10);
-		rviz_interface::publish_arrow_path_unreachable(toTest_start, orthogonal, marker_pub, 2);	
-
-
-
-		toTest_start = octomath::Vector3 (0, 0, 0);
-		toTest_end= octomath::Vector3 (5, 0, 0);
-		orthogonal = getOrthogonalInXNormalized(toTest_start, toTest_end);
-		// octomath::Vector3 orthogonal_rightAnswer (2 3 2);
-		// ASSERT_EQ(orthogonal_rightAnswer, orthogonal);
-		
-
-		rviz_interface::publish_arrow_corridor(toTest_start, toTest_end, marker_pub, 11);
-		rviz_interface::publish_arrow_path_unreachable(toTest_start, orthogonal, marker_pub, 3);	
-	}
-
 	
 	void octomap_callback(const octomap_msgs::Octomap::ConstPtr& octomapBinary){
 		delete octree;
@@ -352,7 +285,8 @@ namespace LazyThetaStarOctree
 		// {
 			
 		learnTf_theHardWay_pointSet_yaw();
-		learnTf_theHardWay_pointSet_roll();
+		// learnTf_theHardWay_pointSet_roll();
+		// learnTf_theHardWay_pointSet_yaw_roll();
 
 
 			// octomath::Vector3 disc_initial(path_request->start.x, path_request->start.y, path_request->start.z);
