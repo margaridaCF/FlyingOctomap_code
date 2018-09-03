dffac01441d0220b416ff872380d66df0523680a
Unknown <margaridaCostaFaria@gmail.com>
2018-09-03 17:29:30 +0200

    [data] Unit tests to collect results.

--- path_planning/src/ltStar_lib.cpp
+++ path_planning/src/ltStar_lib.cpp
@@ -22,8 +22,8 @@ namespace std
 }
 
 namespace LazyThetaStarOctree{
-	std::string folder_name = "/ros_ws/src/data";
-	// std::string folder_name = "/home/mfaria/Flying_Octomap_code/src/data";
+	// std::string folder_name = "/ros_ws/src/data";
+	std::string folder_name = "/home/mfaria/Flying_Octomap_code/src/data";
 
 
 	int obstacle_avoidance_time;
@@ -853,7 +853,7 @@ namespace LazyThetaStarOctree{
 		// ln 18 return "no path found";
 		if(!solution_found)
 		{
-			// ROS_ERROR_STREAM("No solution found. Giving empty path.");
+			ROS_ERROR_STREAM("No solution found. Giving empty path.");
 			if(publish_input.publish)
 			{
         	    log_file <<  "[ltStar] All nodes were analyzed but the final node center " << disc_final_cell_center << " was never reached with " << input.octree.getResolution()/2 << " tolerance. Start " << input.start << ", end " << input.goal << std::endl;
@@ -905,12 +905,12 @@ namespace LazyThetaStarOctree{
 		disc_initial_cell_center = NULL;
 		std::chrono::duration<double> time_lapse = std::chrono::high_resolution_clock::now() - start;
 		int total_in_microseconds = std::chrono::duration_cast<std::chrono::microseconds>(time_lapse).count();
-		ROS_WARN_STREAM("[ltstar] [sparse] Total time " << total_in_microseconds << " microseconds.");
-		ROS_WARN_STREAM("[ltstar] [sparse] generate_neighbors_time took " << generate_neighbors_time << " - " << generate_neighbors_time*100/total_in_microseconds << "%");
-		ROS_WARN_STREAM("[ltstar] [sparse] obstacle_avoidance_time took " << obstacle_avoidance_time << " - " << obstacle_avoidance_time*100.0/total_in_microseconds << "% = " << obstacle_avoidance_time << "*100/" << total_in_microseconds << " = " << obstacle_avoidance_time*100.0 << "/" << total_in_microseconds  );
-		ROS_WARN_STREAM("[ltstar] [sparse] setVertex_time took " << setVertex_time << " - " << setVertex_time*100/total_in_microseconds << "%");
-		ROS_WARN_STREAM("[ltstar] [sparse] updateVertex_time took " << updateVertex_time << " - " << updateVertex_time*100/total_in_microseconds << "%");
-		ROS_WARN_STREAM("[ltstar] [sparse] obstacle_avoidance_calls count " << obstacle_avoidance_calls  );
+		// ROS_WARN_STREAM("[ltstar] [sparse] Total time " << total_in_microseconds << " microseconds.");
+		// ROS_WARN_STREAM("[ltstar] [sparse] generate_neighbors_time took " << generate_neighbors_time << " - " << generate_neighbors_time*100/total_in_microseconds << "%");
+		// ROS_WARN_STREAM("[ltstar] [sparse] obstacle_avoidance_time took " << obstacle_avoidance_time << " - " << obstacle_avoidance_time*100.0/total_in_microseconds << "% = " << obstacle_avoidance_time << "*100/" << total_in_microseconds << " = " << obstacle_avoidance_time*100.0 << "/" << total_in_microseconds  );
+		// ROS_WARN_STREAM("[ltstar] [sparse] setVertex_time took " << setVertex_time << " - " << setVertex_time*100/total_in_microseconds << "%");
+		// ROS_WARN_STREAM("[ltstar] [sparse] updateVertex_time took " << updateVertex_time << " - " << updateVertex_time*100/total_in_microseconds << "%");
+		// ROS_WARN_STREAM("[ltstar] [sparse] obstacle_avoidance_calls count " << obstacle_avoidance_calls  );
 		return path;
 	}
 	// ln 19 end
--- path_planning/test/collect_results.cpp
+++ path_planning/test/collect_results.cpp
@@ -3,114 +3,62 @@
 
 namespace LazyThetaStarOctree
 {
-	timespec diff(timespec start, timespec end)
+    void testResults(path_planning_msgs::LTStarRequest request, octomap::OcTree & octree, std::string dataset_name)
     {
-        timespec temp;
-        if ((end.tv_nsec-start.tv_nsec)<0) {
-            temp.tv_sec = end.tv_sec-start.tv_sec-1;
-            // temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
-        } else {
-            temp.tv_sec = end.tv_sec-start.tv_sec;
-            temp.tv_nsec = end.tv_nsec-start.tv_nsec;
-        }
-        return temp;
+		ros::Publisher marker_pub;
+		path_planning_msgs::LTStarReply reply;
+		double sidelength_lookup_table  [octree.getTreeDepth()];
+	   	LazyThetaStarOctree::fillLookupTable(octree.getResolution(), octree.getTreeDepth(), sidelength_lookup_table); 
+
+		bool success = true;
+		processLTStarRequest(octree, request, reply, sidelength_lookup_table, PublishingInput(marker_pub, true, dataset_name)  );
+		int waypoint_amount = reply.waypoint_amount;
+		ASSERT_EQ(reply.success, success);
+		for (int i = 0; i < 1; ++i)
+		{
+			processLTStarRequest(octree, request, reply, sidelength_lookup_table, PublishingInput(marker_pub, true, dataset_name)  );
+			ASSERT_EQ(reply.success, success);
+			EXPECT_EQ(reply.waypoint_amount, waypoint_amount);
+			EXPECT_EQ(0, ThetaStarNode::OustandingObjects());
+		}
     }
 
-    TEST(LazyThetaStarTests, LazyThetaStar_20180823_1110_1)
+ //    TEST(LazyThetaStarTests, LazyThetaStar_20180821_1110_42712_raw_1)
+	// {
+	// 	octomap::OcTree octree ("data/20180821_1110_42712_raw.bt");
+	// 	path_planning_msgs::LTStarRequest request;
+	// 	request.header.seq = 2;
+	// 	request.request_id = 3;
+	// 	request.start.x = -3.9;
+	// 	request.start.y = -14.76;
+	// 	request.start.z = 7;
+	// 	request.goal.x = 7.49;
+	// 	request.goal.y = -6.98;
+	// 	request.goal.z = 7;
+	// 	request.max_search_iterations = 120;
+	// 	request.safety_margin = 5;
+	// 	testResults(request, octree, "20180821_1110_42712_raw" );
+	// }
+ //    TEST(LazyThetaStarTests, LazyThetaStar_20180821_1110_42712_raw_2)
+	// {
+	// 	octomap::OcTree octree ("data/20180821_1110_42712_raw.bt");
+	// 	path_planning_msgs::LTStarRequest request;
+	// 	request.header.seq = 2;
+	// 	request.request_id = 3;
+	// 	request.start.x = 7.49;
+	// 	request.start.y = -6.98;
+	// 	request.start.z = 7;
+	// 	request.goal.x = -3.9;
+	// 	request.goal.y = -14.76;
+	// 	request.goal.z = 7;
+	// 	request.max_search_iterations = 120;
+	// 	request.safety_margin = 5;
+	// 	testResults(request, octree, "20180821_1110_42712_raw" );
+	// }
+
+    TEST(LazyThetaStarTests, LazyThetaStar_20180821_1110_42712_filtered_1)
 	{
-		ros::Publisher marker_pub;
-		// (0.420435 0.313896 1.92169) to (-2.5 -10.5 3.5)
-		octomap::OcTree octree ("data/20180823_1110_manyRuns_karting.bt");
-		double sidelength_lookup_table  [octree.getTreeDepth()];
-	   	LazyThetaStarOctree::fillLookupTable(octree.getResolution(), octree.getTreeDepth(), sidelength_lookup_table); 
-		path_planning_msgs::LTStarRequest request;
-		request.header.seq = 2;
-		request.request_id = 3;
-		request.start.x = 6;
-		request.start.y = -6;
-		request.start.z = 7;
-		request.goal.x = 7.49;
-		request.goal.y = -6.98;
-		request.goal.z = 7;
-		request.max_search_iterations = 120;
-		request.safety_margin = 5;
-		path_planning_msgs::LTStarReply reply;
-
-		for (int i = 0; i < 10; ++i)
-		{
-			processLTStarRequest(octree, request, reply, sidelength_lookup_table, PublishingInput(marker_pub, true, "20180823_1110_manyRuns_karting")  );
-			ASSERT_TRUE(reply.success);
-			ASSERT_EQ(reply.waypoint_amount, 2);
-			ASSERT_EQ(0, ThetaStarNode::OustandingObjects());
-		}
-	}
-
-    TEST(LazyThetaStarTests, LazyThetaStar_20180823_1110_filtered_1)
-	{
-		ros::Publisher marker_pub;
-		// (0.420435 0.313896 1.92169) to (-2.5 -10.5 3.5)
-		octomap::OcTree octree ("data/20180823_1110_manyRuns_karting_filterIntensityRange.bt");
-		double sidelength_lookup_table  [octree.getTreeDepth()];
-	   	LazyThetaStarOctree::fillLookupTable(octree.getResolution(), octree.getTreeDepth(), sidelength_lookup_table); 
-		path_planning_msgs::LTStarRequest request;
-		request.header.seq = 2;
-		request.request_id = 3;
-		request.start.x = 6;
-		request.start.y = -6;
-		request.start.z = 7;
-		request.goal.x = 7.49;
-		request.goal.y = -6.98;
-		request.goal.z = 7;
-		request.max_search_iterations = 120;
-		request.safety_margin = 5;
-		path_planning_msgs::LTStarReply reply;
-
-		for (int i = 0; i < 10; ++i)
-		{
-			processLTStarRequest(octree, request, reply, sidelength_lookup_table, PublishingInput(marker_pub, true, "20180823_1110_manyRuns_karting_filterIntensityRange")  );
-			ASSERT_TRUE(reply.success);
-			ASSERT_EQ(reply.waypoint_amount, 2);
-			ASSERT_EQ(0, ThetaStarNode::OustandingObjects());
-		}
-	}
-
-
-    TEST(LazyThetaStarTests, LazyThetaStar_20180823_1110_2)
-	{
-		ros::Publisher marker_pub;
-		// (0.420435 0.313896 1.92169) to (-2.5 -10.5 3.5)
-		octomap::OcTree octree ("data/20180823_1110_manyRuns_karting.bt");
-		double sidelength_lookup_table  [octree.getTreeDepth()];
-	   	LazyThetaStarOctree::fillLookupTable(octree.getResolution(), octree.getTreeDepth(), sidelength_lookup_table); 
-		path_planning_msgs::LTStarRequest request;
-		request.header.seq = 2;
-		request.request_id = 3;
-		request.start.x = -3.9;
-		request.start.y = -14.76;
-		request.start.z = 7;
-		request.goal.x = 7.49;
-		request.goal.y = -6.98;
-		request.goal.z = 7;
-		request.max_search_iterations = 120;
-		request.safety_margin = 7;
-		path_planning_msgs::LTStarReply reply;
-
-		for (int i = 0; i < 10; ++i)
-		{
-			processLTStarRequest(octree, request, reply, sidelength_lookup_table, PublishingInput(marker_pub, true, "20180823_1110_manyRuns_karting")  );
-			ASSERT_TRUE(reply.success);
-			ASSERT_EQ(reply.waypoint_amount, 5);
-			ASSERT_EQ(0, ThetaStarNode::OustandingObjects());
-		}
-	}
-
-    TEST(LazyThetaStarTests, LazyThetaStar_20180823_1110_filtered_2)
-	{
-		ros::Publisher marker_pub;
-		// (0.420435 0.313896 1.92169) to (-2.5 -10.5 3.5)
-		octomap::OcTree octree ("data/20180823_1110_manyRuns_karting_filterIntensityRange.bt");
-		double sidelength_lookup_table  [octree.getTreeDepth()];
-	   	LazyThetaStarOctree::fillLookupTable(octree.getResolution(), octree.getTreeDepth(), sidelength_lookup_table); 
+		octomap::OcTree octree ("data/20180821_1110_42712_filtered.bt");
 		path_planning_msgs::LTStarRequest request;
 		request.header.seq = 2;
 		request.request_id = 3;
@@ -122,25 +70,11 @@ namespace LazyThetaStarOctree
 		request.goal.z = 7;
 		request.max_search_iterations = 120;
 		request.safety_margin = 5;
-		path_planning_msgs::LTStarReply reply;
-
-		for (int i = 0; i < 10; ++i)
-		{
-			processLTStarRequest(octree, request, reply, sidelength_lookup_table, PublishingInput(marker_pub, true, "20180823_1110_manyRuns_karting_filterIntensityRange")  );
-			ASSERT_TRUE(reply.success);
-			ASSERT_EQ(reply.waypoint_amount, 7);
-			ASSERT_EQ(0, ThetaStarNode::OustandingObjects());
-		}
+		testResults(request, octree, "20180821_1110_42712_filtered" );
 	}
-
-
-    TEST(LazyThetaStarTests, LazyThetaStar_20180823_1110_3)
+    TEST(LazyThetaStarTests, LazyThetaStar_20180821_1110_42712_filtered_2)
 	{
-		ros::Publisher marker_pub;
-		// (0.420435 0.313896 1.92169) to (-2.5 -10.5 3.5)
-		octomap::OcTree octree ("data/20180823_1110_manyRuns_karting.bt");
-		double sidelength_lookup_table  [octree.getTreeDepth()];
-	   	LazyThetaStarOctree::fillLookupTable(octree.getResolution(), octree.getTreeDepth(), sidelength_lookup_table); 
+		octomap::OcTree octree ("data/20180821_1110_42712_filtered.bt");
 		path_planning_msgs::LTStarRequest request;
 		request.header.seq = 2;
 		request.request_id = 3;
@@ -152,45 +86,145 @@ namespace LazyThetaStarOctree
 		request.goal.z = 7;
 		request.max_search_iterations = 120;
 		request.safety_margin = 5;
-		path_planning_msgs::LTStarReply reply;
-
-		for (int i = 0; i < 10; ++i)
-		{
-			processLTStarRequest(octree, request, reply, sidelength_lookup_table, PublishingInput(marker_pub, true, "20180823_1110_manyRuns_karting")  );
-			ASSERT_TRUE(reply.success);
-			ASSERT_EQ(reply.waypoint_amount, 5);
-			ASSERT_EQ(0, ThetaStarNode::OustandingObjects());
-		}
+		testResults(request, octree, "20180821_1110_42712_filtered" );
 	}
 
-    TEST(LazyThetaStarTests, LazyThetaStar_20180823_1110_filtered_3)
-	{
-		ros::Publisher marker_pub;
-		// (0.420435 0.313896 1.92169) to (-2.5 -10.5 3.5)
-		octomap::OcTree octree ("data/20180823_1110_manyRuns_karting_filterIntensityRange.bt");
-		double sidelength_lookup_table  [octree.getTreeDepth()];
-	   	LazyThetaStarOctree::fillLookupTable(octree.getResolution(), octree.getTreeDepth(), sidelength_lookup_table); 
-		path_planning_msgs::LTStarRequest request;
-		request.header.seq = 2;
-		request.request_id = 3;
-		request.start.x = 7.49;
-		request.start.y = -6.98;
-		request.start.z = 7;
-		request.goal.x = -3.9;
-		request.goal.y = -14.76;
-		request.goal.z = 7;
-		request.max_search_iterations = 120;
-		request.safety_margin = 5;
-		path_planning_msgs::LTStarReply reply;
 
-		for (int i = 0; i < 10; ++i)
-		{
-			processLTStarRequest(octree, request, reply, sidelength_lookup_table, PublishingInput(marker_pub, true, "20180823_1110_manyRuns_karting_filterIntensityRange")  );
-			ASSERT_TRUE(reply.success);
-			ASSERT_EQ(reply.waypoint_amount, 6);
-			ASSERT_EQ(0, ThetaStarNode::OustandingObjects());
-		}
-	}
+ //    TEST(LazyThetaStarTests, LazyThetaStar_20180821_1110_42936_raw_1)
+	// {
+	// 	octomap::OcTree octree ("data/20180821_1110_42936_raw.bt");
+	// 	path_planning_msgs::LTStarRequest request;
+	// 	request.header.seq = 2;
+	// 	request.request_id = 3;
+	// 	request.start.x = -3.9;
+	// 	request.start.y = -14.76;
+	// 	request.start.z = 7;
+	// 	request.goal.x = 7.49;
+	// 	request.goal.y = -6.98;
+	// 	request.goal.z = 7;
+	// 	request.max_search_iterations = 120;
+	// 	request.safety_margin = 5;
+	// 	testResults(request, octree, "20180821_1110_42936_raw" );
+	// }
+ //    TEST(LazyThetaStarTests, LazyThetaStar_20180821_1110_42936_raw_2)
+	// {
+	// 	octomap::OcTree octree ("data/20180821_1110_42936_raw.bt");
+	// 	path_planning_msgs::LTStarRequest request;
+	// 	request.header.seq = 2;
+	// 	request.request_id = 3;
+	// 	request.start.x = 7.49;
+	// 	request.start.y = -6.98;
+	// 	request.start.z = 7;
+	// 	request.goal.x = -3.9;
+	// 	request.goal.y = -14.76;
+	// 	request.goal.z = 7;
+	// 	request.max_search_iterations = 120;
+	// 	request.safety_margin = 5;
+	// 	testResults(request, octree, "20180821_1110_42936_raw" );
+	// }
+
+ //    TEST(LazyThetaStarTests, LazyThetaStar_20180821_1110_42936_filtered_1)
+	// {
+	// 	octomap::OcTree octree ("data/20180821_1110_42936_filtered.bt");
+	// 	path_planning_msgs::LTStarRequest request;
+	// 	request.header.seq = 2;
+	// 	request.request_id = 3;
+	// 	request.start.x = -3.9;
+	// 	request.start.y = -14.76;
+	// 	request.start.z = 7;
+	// 	request.goal.x = 7.49;
+	// 	request.goal.y = -6.98;
+	// 	request.goal.z = 7;
+	// 	request.max_search_iterations = 120;
+	// 	request.safety_margin = 5;
+	// 	testResults(request, octree, "20180821_1110_42936_filtered" );
+	// }
+ //    TEST(LazyThetaStarTests, LazyThetaStar_20180821_1110_42936_filtered_2)
+	// {
+	// 	octomap::OcTree octree ("data/20180821_1110_42936_filtered.bt");
+	// 	path_planning_msgs::LTStarRequest request;
+	// 	request.header.seq = 2;
+	// 	request.request_id = 3;
+	// 	request.start.x = 7.49;
+	// 	request.start.y = -6.98;
+	// 	request.start.z = 7;
+	// 	request.goal.x = -3.9;
+	// 	request.goal.y = -14.76;
+	// 	request.goal.z = 7;
+	// 	request.max_search_iterations = 120;
+	// 	request.safety_margin = 5;
+	// 	testResults(request, octree, "20180821_1110_42936_filtered" );
+	// }
+
+
+
+
+ //    TEST(LazyThetaStarTests, LazyThetaStar_20180821_1110_43042_raw_1)
+	// {
+	// 	octomap::OcTree octree ("data/20180821_1110_43042_raw.bt");
+	// 	path_planning_msgs::LTStarRequest request;
+	// 	request.header.seq = 2;
+	// 	request.request_id = 3;
+	// 	request.start.x = -3.9;
+	// 	request.start.y = -14.76;
+	// 	request.start.z = 7;
+	// 	request.goal.x = 7.49;
+	// 	request.goal.y = -6.98;
+	// 	request.goal.z = 7;
+	// 	request.max_search_iterations = 120;
+	// 	request.safety_margin = 5;
+	// 	testResults(request, octree, "20180821_1110_43042_raw" );
+	// }
+ //    TEST(LazyThetaStarTests, LazyThetaStar_20180821_1110_43042_raw_2)
+	// {
+	// 	octomap::OcTree octree ("data/20180821_1110_43042_raw.bt");
+	// 	path_planning_msgs::LTStarRequest request;
+	// 	request.header.seq = 2;
+	// 	request.request_id = 3;
+	// 	request.start.x = 7.49;
+	// 	request.start.y = -6.98;
+	// 	request.start.z = 7;
+	// 	request.goal.x = -3.9;
+	// 	request.goal.y = -14.76;
+	// 	request.goal.z = 7;
+	// 	request.max_search_iterations = 120;
+	// 	request.safety_margin = 5;
+	// 	testResults(request, octree, "20180821_1110_43042_raw" );
+	// }
+
+ //    TEST(LazyThetaStarTests, LazyThetaStar_20180821_1110_43042_filtered_1)
+	// {
+	// 	octomap::OcTree octree ("data/20180821_1110_43042_filtered.bt");
+	// 	path_planning_msgs::LTStarRequest request;
+	// 	request.header.seq = 2;
+	// 	request.request_id = 3;
+	// 	request.start.x = -3.9;
+	// 	request.start.y = -14.76;
+	// 	request.start.z = 7;
+	// 	request.goal.x = 7.49;
+	// 	request.goal.y = -6.98;
+	// 	request.goal.z = 7;
+	// 	request.max_search_iterations = 120;
+	// 	request.safety_margin = 5;
+	// 	testResults(request, octree, "20180821_1110_43042_filtered");
+	// }
+ //    TEST(LazyThetaStarTests, LazyThetaStar_20180821_1110_43042_filtered_2)
+	// {
+	// 	octomap::OcTree octree ("data/20180821_1110_43042_filtered.bt");
+	// 	path_planning_msgs::LTStarRequest request;
+	// 	request.header.seq = 2;
+	// 	request.request_id = 3;
+	// 	request.start.x = 7.49;
+	// 	request.start.y = -6.98;
+	// 	request.start.z = 7;
+	// 	request.goal.x = -3.9;
+	// 	request.goal.y = -14.76;
+	// 	request.goal.z = 7;
+	// 	request.max_search_iterations = 120;
+	// 	request.safety_margin = 5;
+	// 	testResults(request, octree, "20180821_1110_43042_filtered");
+	// }
+
 }
 
 int main(int argc, char **argv){
