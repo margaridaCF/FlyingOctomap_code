5d97552eabc234847b3068ecd0a238fc85a64dd7
Unknown <margaridaCostaFaria@gmail.com>
2018-09-05 16:56:16 +0200

    [ortho] Passing older tests.

--- path_planning/test/orthogonal_planes_tests.cpp
+++ path_planning/test/orthogonal_planes_tests.cpp
@@ -18,236 +18,236 @@ namespace LazyThetaStarOctree{
 	}
 
 
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToYaxis)
-	// {
-	// 	octomath::Vector3 start (0, 0, 0);
-	// 	octomath::Vector3 goal  (0, 1, 0);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToNegatativeYaxis)
-	// {
-	// 	octomath::Vector3 start (0, 0, 0);
-	// 	octomath::Vector3 goal  (0, -1, 0);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToXaxis)
-	// {
-	// 	octomath::Vector3 start (0, 0, 0);
-	// 	octomath::Vector3 goal  (1, 0, 0);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToNegatativeXaxis)
-	// {
-	// 	octomath::Vector3 start (0, 0, 0);
-	// 	octomath::Vector3 goal  (-1, 0, 0);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToZaxis)
-	// {
-	// 	octomath::Vector3 start (0, 0, 0);
-	// 	octomath::Vector3 goal  (0, 0, 1);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_closeToOriginToZaxis)
-	// {
-	// 	octomath::Vector3 start (0, 0, 0);
-	// 	octomath::Vector3 goal  (0.1, 0, 1);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToNegatativeZaxis)
-	// {
-	// 	octomath::Vector3 start (0, 0, 0);
-	// 	octomath::Vector3 goal  (0, 0, -1);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToXaxisZero)
-	// {
-	// 	octomath::Vector3 start (0, 0, 0);
-	// 	octomath::Vector3 goal  (0, 1, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(0, -1, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(0, -1, -1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(0, 1, -1);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToYaxisZero)
-	// {
-	// 	octomath::Vector3 start (0, 0, 0);
-	// 	octomath::Vector3 goal  (1, 0, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(-1, 0, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(-1, 0, -1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(1, 0, -1);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToAllDiagonals)
-	// {
-	// 	octomath::Vector3 start (0, 0, 0);
-	// 	octomath::Vector3 goal  (1, 1, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(-1, 1, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(-1, 1, -1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(1, 1, -1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(1, -1, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(-1, -1, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(-1, -1, -1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(1, -1, -1);
-	// 	testCoordinateFrame(start, goal);
-	// }
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToYaxis)
+	{
+		octomath::Vector3 start (0, 0, 0);
+		octomath::Vector3 goal  (0, 1, 0);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToNegatativeYaxis)
+	{
+		octomath::Vector3 start (0, 0, 0);
+		octomath::Vector3 goal  (0, -1, 0);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToXaxis)
+	{
+		octomath::Vector3 start (0, 0, 0);
+		octomath::Vector3 goal  (1, 0, 0);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToNegatativeXaxis)
+	{
+		octomath::Vector3 start (0, 0, 0);
+		octomath::Vector3 goal  (-1, 0, 0);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToZaxis)
+	{
+		octomath::Vector3 start (0, 0, 0);
+		octomath::Vector3 goal  (0, 0, 1);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_closeToOriginToZaxis)
+	{
+		octomath::Vector3 start (0, 0, 0);
+		octomath::Vector3 goal  (0.1, 0, 1);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToNegatativeZaxis)
+	{
+		octomath::Vector3 start (0, 0, 0);
+		octomath::Vector3 goal  (0, 0, -1);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToXaxisZero)
+	{
+		octomath::Vector3 start (0, 0, 0);
+		octomath::Vector3 goal  (0, 1, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(0, -1, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(0, -1, -1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(0, 1, -1);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToYaxisZero)
+	{
+		octomath::Vector3 start (0, 0, 0);
+		octomath::Vector3 goal  (1, 0, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(-1, 0, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(-1, 0, -1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(1, 0, -1);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_originToAllDiagonals)
+	{
+		octomath::Vector3 start (0, 0, 0);
+		octomath::Vector3 goal  (1, 1, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(-1, 1, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(-1, 1, -1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(1, 1, -1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(1, -1, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(-1, -1, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(-1, -1, -1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(1, -1, -1);
+		testCoordinateFrame(start, goal);
+	}
 
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToYaxis)
-	// {
-	// 	octomath::Vector3 start (2, 5, -634);
-	// 	octomath::Vector3 goal  (0, 1, 0);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToNegatativeYaxis)
-	// {
-	// 	octomath::Vector3 start (2, 5, -634);
-	// 	octomath::Vector3 goal  (0, -1, 0);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToXaxis)
-	// {
-	// 	octomath::Vector3 start (2, 5, -634);
-	// 	octomath::Vector3 goal  (1, 0, 0);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToNegatativeXaxis)
-	// {
-	// 	octomath::Vector3 start (2, 5, -634);
-	// 	octomath::Vector3 goal  (-1, 0, 0);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToZaxis)
-	// {
-	// 	octomath::Vector3 start (2, 5, -634);
-	// 	octomath::Vector3 goal  (0, 0, 1);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_closeToOriginToZaxis)
-	// {
-	// 	octomath::Vector3 start (2, 5, -634);
-	// 	octomath::Vector3 goal  (0.1, 0, 1);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToNegatativeZaxis)
-	// {
-	// 	octomath::Vector3 start (2, 5, -634);
-	// 	octomath::Vector3 goal  (0, 0, -1);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToXaxisZero)
-	// {
-	// 	octomath::Vector3 start (2, 5, -634);
-	// 	octomath::Vector3 goal  (0, 1, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(0, -1, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(0, -1, -1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(0, 1, -1);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToYaxisZero)
-	// {
-	// 	octomath::Vector3 start (2, 5, -634);
-	// 	octomath::Vector3 goal  (1, 0, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(-1, 0, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(-1, 0, -1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(1, 0, -1);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToAllDiagonals)
-	// {
-	// 	octomath::Vector3 start (2, 5, -634);
-	// 	octomath::Vector3 goal  (1, 1, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(-1, 1, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(-1, 1, -1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(1, 1, -1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(1, -1, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(-1, -1, 1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(-1, -1, -1);
-	// 	testCoordinateFrame(start, goal);
-	// 	goal = octomath::Vector3(1, -1, -1);
-	// 	testCoordinateFrame(start, goal);
-	// }
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToYaxis)
+	{
+		octomath::Vector3 start (2, 5, -634);
+		octomath::Vector3 goal  (0, 1, 0);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToNegatativeYaxis)
+	{
+		octomath::Vector3 start (2, 5, -634);
+		octomath::Vector3 goal  (0, -1, 0);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToXaxis)
+	{
+		octomath::Vector3 start (2, 5, -634);
+		octomath::Vector3 goal  (1, 0, 0);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToNegatativeXaxis)
+	{
+		octomath::Vector3 start (2, 5, -634);
+		octomath::Vector3 goal  (-1, 0, 0);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToZaxis)
+	{
+		octomath::Vector3 start (2, 5, -634);
+		octomath::Vector3 goal  (0, 0, 1);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_closeToOriginToZaxis)
+	{
+		octomath::Vector3 start (2, 5, -634);
+		octomath::Vector3 goal  (0.1, 0, 1);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToNegatativeZaxis)
+	{
+		octomath::Vector3 start (2, 5, -634);
+		octomath::Vector3 goal  (0, 0, -1);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToXaxisZero)
+	{
+		octomath::Vector3 start (2, 5, -634);
+		octomath::Vector3 goal  (0, 1, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(0, -1, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(0, -1, -1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(0, 1, -1);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToYaxisZero)
+	{
+		octomath::Vector3 start (2, 5, -634);
+		octomath::Vector3 goal  (1, 0, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(-1, 0, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(-1, 0, -1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(1, 0, -1);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_offset_originToAllDiagonals)
+	{
+		octomath::Vector3 start (2, 5, -634);
+		octomath::Vector3 goal  (1, 1, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(-1, 1, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(-1, 1, -1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(1, 1, -1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(1, -1, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(-1, -1, 1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(-1, -1, -1);
+		testCoordinateFrame(start, goal);
+		goal = octomath::Vector3(1, -1, -1);
+		testCoordinateFrame(start, goal);
+	}
 
 
 
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest)
-	// {
-	// 	octomath::Vector3 start (2, 5, -634);
-	// 	octomath::Vector3 goal  (20, -31, 40);
-	// 	testCoordinateFrame(start, goal);
-	// }
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest)
+	{
+		octomath::Vector3 start (2, 5, -634);
+		octomath::Vector3 goal  (20, -31, 40);
+		testCoordinateFrame(start, goal);
+	}
 
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest1)
-	// {
-	// 	octomath::Vector3 start (2, 5, -634);
-	// 	octomath::Vector3 goal  (20, -31, 40);
-	// 	testCoordinateFrame(start, goal);
-	// }
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest1)
+	{
+		octomath::Vector3 start (2, 5, -634);
+		octomath::Vector3 goal  (20, -31, 40);
+		testCoordinateFrame(start, goal);
+	}
 
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest2)
-	// {
-	// 	octomath::Vector3 start (-2, 785, -634);
-	// 	octomath::Vector3 goal  (20, -31, 40);
-	// 	testCoordinateFrame(start, goal);
-	// }
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest2)
+	{
+		octomath::Vector3 start (-2, 785, -634);
+		octomath::Vector3 goal  (20, -31, 40);
+		testCoordinateFrame(start, goal);
+	}
 
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest3)
-	// {
-	// 	octomath::Vector3 start (-2, 5, -634);
-	// 	octomath::Vector3 goal  (20, -31, -40);
-	// 	testCoordinateFrame(start, goal);
-	// }
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest4)
-	// {
-	// 	octomath::Vector3 start (2, 25, -634);
-	// 	octomath::Vector3 goal  (20, -31, 40);
-	// 	testCoordinateFrame(start, goal);
-	// }
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest3)
+	{
+		octomath::Vector3 start (-2, 5, -634);
+		octomath::Vector3 goal  (20, -31, -40);
+		testCoordinateFrame(start, goal);
+	}
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest4)
+	{
+		octomath::Vector3 start (2, 25, -634);
+		octomath::Vector3 goal  (20, -31, 40);
+		testCoordinateFrame(start, goal);
+	}
 
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest5)
-	// {
-	// 	octomath::Vector3 start (2, -5, -634);
-	// 	octomath::Vector3 goal  (20, -31, 40);
-	// 	testCoordinateFrame(start, goal);
-	// }
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest5)
+	{
+		octomath::Vector3 start (2, -5, -634);
+		octomath::Vector3 goal  (20, -31, 40);
+		testCoordinateFrame(start, goal);
+	}
 
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest6)
-	// {
-	// 	octomath::Vector3 start (-2, 785, -634);
-	// 	octomath::Vector3 goal  (-20, -31, 40);
-	// 	testCoordinateFrame(start, goal);
-	// }
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest6)
+	{
+		octomath::Vector3 start (-2, 785, -634);
+		octomath::Vector3 goal  (-20, -31, 40);
+		testCoordinateFrame(start, goal);
+	}
 
-	// TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest7)
-	// {
-	// 	octomath::Vector3 start (-2, -5, -634);
-	// 	octomath::Vector3 goal  (-20, -31, -40);
-	// 	testCoordinateFrame(start, goal);
-	// }
+	TEST(OrthogonalPlanesTest, generateCoordinateFrame_monkeyTest7)
+	{
+		octomath::Vector3 start (-2, -5, -634);
+		octomath::Vector3 goal  (-20, -31, -40);
+		testCoordinateFrame(start, goal);
+	}
 
 	// TEST(OrthogonalPlanesTest, generategoalWithDistance_)
 	// {
