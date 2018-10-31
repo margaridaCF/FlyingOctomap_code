76d7bfbc53c0d4f7c343247622c9c603ee48ce6e
Meg <margaridaCostaFaria@gmail.com>
2018-10-31 15:47:45 +0100

    [plotly] Success rate.

--- _generate_plots/success_rate.py
+++ _generate_plots/success_rate.py
@@ -9,6 +9,12 @@ import csv
 
 def calculate_success_rate(data, margin):
 	count_success = data["success"][(data["success"] == 1) & (data["safety_margin_meters"] == margin) ].count()
+	print count_success
 	count_total = data["success"][(data["safety_margin_meters"] == 5) ].count()
 	success_rate = count_success * 100 / count_total
 	return success_rate
+
+def load_success_rate(csv_filepath):
+	data = pd.read_csv(csv_filepath)
+	margins = [3.9, 5, 5.4]
+	return  [calculate_success_rate(data, margin) for margin in margins]
