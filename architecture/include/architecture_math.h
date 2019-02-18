b2084a0aadd88069e6b716c23e2b7315939185d3
Unknown <margaridaCostaFaria@gmail.com>
2019-02-18 18:04:45 +0000

    [architecture] Tests for the orientation.

--- architecture/include/architecture_math.h
+++ architecture/include/architecture_math.h
@@ -1,18 +1,36 @@
 #ifndef ARCHITECTURE_MATH_H
 #define ARCHITECTURE_MATH_H
 #include <Eigen/Dense>
+#include <ros/ros.h>
 
 
 namespace architecture_math
 {
 
 
-	double calculateOrientation(Eigen::Vector3d start, Eigen::Vector3d end)
-    {
-        Eigen::Vector3d d = end - start;
-        d.normalize();
-        return std::acos (1/d.norm());
-    }
+	double calculateOrientation(Eigen::Vector2d start, Eigen::Vector2d end)
+	{
+	   	double result;
+		Eigen::Vector2d d = end - start;
+		double hipotenuse = d.norm();
+		double adjacent = end.x() - start.x();
+		if(d.norm() == 0)
+		{
+			result = 0;
+		} 	
+		else
+		{
+			result = adjacent/hipotenuse ;
+			result = std::acos (result);
+			d.normalize();
+			if(d.y() < 0)
+			{
+				result = -result; 
+			}
+		} 				
+		return result;
+	}
+
 
 }
 #endif // ARCHITECTURE_MATH_h
