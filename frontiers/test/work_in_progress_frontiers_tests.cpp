599291690df72958cce75d0013db7fe190f18031
Unknown <margaridaCostaFaria@gmail.com>
2019-06-02 19:12:46 +0100

    [csv] Calculate volume explored. Min geofence with tests.

--- frontiers/test/work_in_progress_frontiers_tests.cpp
+++ frontiers/test/work_in_progress_frontiers_tests.cpp
@@ -1,9 +1,8 @@
 #include <gtest/gtest.h>
-#include <next_best_view.h>
 #include <frontiers.h>
 #include <neighbors.h>
 
-namespace Frontiers
+namespace Frontiers_test
 {
 	void printForMatlab(std::unordered_set<std::shared_ptr<octomath::Vector3>> neighbors)
 	{
@@ -18,6 +17,117 @@ namespace Frontiers
 		}
 	}
 
+	double sizeInsideGeofence_min(double side_min, double side_max, double size, double min)
+	{
+		double how_much;
+		bool is_inside = side_max > min; 
+		ROS_INFO_STREAM("is_inside: side_max > min <==> " << side_max << " > " << min);
+		if(is_inside)
+		{
+			bool all_in = side_min >= min;
+			ROS_INFO_STREAM("All in? " << side_min << " > " << min);
+			if (all_in)
+			{
+				ROS_INFO_STREAM("Yup, all in!");
+				return size;
+			}
+			else
+			{
+				how_much = std::abs(min - side_max);
+				ROS_INFO_STREAM("Nope, just " << how_much << " = std::abs(" << min << " - " << side_max << ")");
+				return how_much;
+			}
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+
+
+
+	double volumeInsideGeofence(octomath::Vector3 const& min, octomath::Vector3 const& max, octomap::OcTree::leaf_bbx_iterator const& it)
+	{
+		octomath::Vector3 side_max = it.getCoordinate() + octomath::Vector3(it.getSize()/2, it.getSize()/2, it.getSize()/2); 
+		octomath::Vector3 side_min = it.getCoordinate() - octomath::Vector3(it.getSize()/2, it.getSize()/2, it.getSize()/2); 
+		ROS_INFO_STREAM("Center: " << it.getCoordinate() << " size " << it.getSize() << ". Sides " << side_min << " to " << side_max << ". Geofence Min " << min);
+		double how_much;
+		bool is_inside = side_max.x() > min.x(); 
+		double side_x = sizeInsideGeofence_min(side_min.x(), side_max.x(), it.getSize(), min.x());
+		return side_x;
+		// ROS_INFO_STREAM("is_inside: side_max > min <==> " << side_max.x() << " > " << min.x());
+		// if(is_inside)
+		// {
+		// 	bool all_in = side_min.x() >= min.x();
+		// 	ROS_INFO_STREAM("All in? " << side_min.x() << " > " << min.x());
+		// 	if (all_in)
+		// 	{
+		// 		ROS_INFO_STREAM("Yup, all in!");
+		// 		return it.getSize();
+		// 	}
+		// 	else
+		// 	{
+		// 		how_much = std::abs(min.x() - side_max.x());
+		// 		ROS_INFO_STREAM("Nope, just " << how_much << " = std::abs(" << min.x() << " - " << side_max.x() << ")");
+		// 		return how_much;
+		// 	}
+		// }
+		// else
+		// {
+		// 	return 0;
+		// }
+
+	}
+
+	TEST(VolumeTest, OutsideOnBorderGeofence)
+	{
+		octomap::OcTree octree ("data/experimentalDataset.bt");
+		octomath::Vector3 min (0, 0, 0);
+		octomath::Vector3 max (1, 1, 1);
+
+		octomap::OcTreeKey bbxMinKey, bbxMaxKey;
+        if(!octree.coordToKeyChecked(min, bbxMinKey) || !octree.coordToKeyChecked(max, bbxMaxKey))
+        {
+            ROS_ERROR_STREAM("[Frontiers] Problems with write_volume_explored_to_csv");
+        }
+		octomap::OcTree::leaf_bbx_iterator it = octree.begin_leafs_bbx(bbxMinKey,bbxMaxKey);
+		ASSERT_EQ(volumeInsideGeofence (min, max, it), 0);
+	}
+
+	TEST(VolumeTest, InsideOnBorderGeofence)
+	{
+		octomap::OcTree octree ("data/experimentalDataset.bt");
+		octomath::Vector3 min (0, 0, 0);
+		octomath::Vector3 max (1, 1, 1);
+
+		octomap::OcTreeKey bbxMinKey, bbxMaxKey;
+        if(!octree.coordToKeyChecked(min, bbxMinKey) || !octree.coordToKeyChecked(max, bbxMaxKey))
+        {
+            ROS_ERROR_STREAM("[Frontiers] Problems with write_volume_explored_to_csv");
+        }
+		octomap::OcTree::leaf_bbx_iterator it = octree.begin_leafs_bbx(bbxMinKey,bbxMaxKey);
+		it++;
+		ASSERT_NEAR(volumeInsideGeofence (min, max, it), 0.2, 0.01);
+	}
+
+	TEST(VolumeTest, InsideGeofence)
+	{
+		octomap::OcTree octree ("data/experimentalDataset.bt");
+		octomath::Vector3 min (0, 0, 0);
+		octomath::Vector3 max (1, 1, 1);
+
+		octomap::OcTreeKey bbxMinKey, bbxMaxKey;
+        if(!octree.coordToKeyChecked(min, bbxMinKey) || !octree.coordToKeyChecked(max, bbxMaxKey))
+        {
+            ROS_ERROR_STREAM("[Frontiers] Problems with write_volume_explored_to_csv");
+        }
+		octomap::OcTree::leaf_bbx_iterator it = octree.begin_leafs_bbx(bbxMinKey,bbxMaxKey);
+		it++;
+		it++;
+		ASSERT_NEAR(volumeInsideGeofence (min, max, it), 0.2, 0.01);
+	}
+    
 
 }
 
