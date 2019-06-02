590bbdb9c7678216f85249734f1a365c96bb7eb4
Unknown <margaridaCostaFaria@gmail.com>
2019-06-03 00:25:12 +0100

    [architecture] Remove unused code.

--- architecture/src/current_position_provider_node.cpp
+++ architecture/src/current_position_provider_node.cpp
@@ -14,16 +14,6 @@ namespace current_position_provider_node
 	geometry_msgs::Point current_position;
 	bool current_position_init;
 
-    ros::Publisher marker_pub;
-    visualization_msgs::Marker marker;
-    std::chrono::high_resolution_clock::time_point start;
-
-	void updatePositionMarker()
-	{
-	    marker.points.push_back(current_position);
-	    marker_pub.publish( marker );
-	}
-
 	bool get_current_position(architecture_msgs::PositionMiddleMan::Request &req,
 		architecture_msgs::PositionMiddleMan::Response &res)
 	{
@@ -44,15 +34,6 @@ namespace current_position_provider_node
 	{
 		current_position = new_odometry->pose.position;
 		current_position_init = true;
-
-		std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(now - start);
-		std::chrono::seconds seconds = std::chrono::duration_cast<std::chrono::seconds>(time_span);
-		if( (seconds.count() % 2) == 0)
-		{
-			updatePositionMarker();
-		}
-
 	}
 }
 
@@ -68,11 +49,5 @@ int main(int argc, char **argv)
 	
 	ros::ServiceServer service = nh.advertiseService("get_current_position", current_position_provider_node::get_current_position);
 	ros::Subscriber ground_truth_sub = nh.subscribe<geometry_msgs::PoseStamped>(current_position_topic, 1, current_position_provider_node::ground_truth_cb);
-
-    current_position_provider_node::marker_pub = nh.advertise<visualization_msgs::Marker>("position_log", 1);
-	current_position_provider_node::marker = rviz_interface::createEmptyLineStrip(30);
-	current_position_provider_node::start = std::chrono::high_resolution_clock::now();
-
-	
 	ros::spin();
 }
