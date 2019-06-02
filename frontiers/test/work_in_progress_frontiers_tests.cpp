573b180f09665d3081ac0f65a9bdeff9620ea923
Unknown <margaridaCostaFaria@gmail.com>
2019-06-02 20:12:44 +0100

    [csv] Tests for max geofence.

--- frontiers/test/work_in_progress_frontiers_tests.cpp
+++ frontiers/test/work_in_progress_frontiers_tests.cpp
@@ -44,41 +44,89 @@ namespace Frontiers_test
 		}
 	}
 
-
-
+	double sizeInsideGeofence_max(double side_min, double side_max, double size, double max)
+	{
+		double how_much;
+		bool is_inside = side_min < max; 
+		ROS_INFO_STREAM("is_inside: side_min < max <==> " << side_min << " < " << max);
+		if(is_inside)
+		{
+			bool all_in = side_max <= max;
+			ROS_INFO_STREAM("All in? " << side_max << " <= " << max);
+			if (all_in)
+			{
+				ROS_INFO_STREAM("Yup, all in!");
+				return size;
+			}
+			else
+			{
+				how_much = std::abs(max - side_min);
+				ROS_INFO_STREAM("Nope, just " << how_much << " = std::abs(" << max << " - " << side_min<< ")");
+				return how_much;
+			}
+		}
+		else
+		{
+			return 0;
+		}
+	}
 
 	double volumeInsideGeofence(octomath::Vector3 const& min, octomath::Vector3 const& max, octomap::OcTree::leaf_bbx_iterator const& it)
 	{
 		octomath::Vector3 side_max = it.getCoordinate() + octomath::Vector3(it.getSize()/2, it.getSize()/2, it.getSize()/2); 
 		octomath::Vector3 side_min = it.getCoordinate() - octomath::Vector3(it.getSize()/2, it.getSize()/2, it.getSize()/2); 
-		ROS_INFO_STREAM("Center: " << it.getCoordinate() << " size " << it.getSize() << ". Sides " << side_min << " to " << side_max << ". Geofence Min " << min);
+		ROS_INFO_STREAM("Center: " << it.getCoordinate() << " size " << it.getSize() << ". Sides " << side_min << " to " << side_max << ". Geofence " << min << " to " << max);
 		double how_much;
 		bool is_inside = side_max.x() > min.x(); 
 		double side_x = sizeInsideGeofence_min(side_min.x(), side_max.x(), it.getSize(), min.x());
 		return side_x;
-		// ROS_INFO_STREAM("is_inside: side_max > min <==> " << side_max.x() << " > " << min.x());
-		// if(is_inside)
-		// {
-		// 	bool all_in = side_min.x() >= min.x();
-		// 	ROS_INFO_STREAM("All in? " << side_min.x() << " > " << min.x());
-		// 	if (all_in)
-		// 	{
-		// 		ROS_INFO_STREAM("Yup, all in!");
-		// 		return it.getSize();
-		// 	}
-		// 	else
-		// 	{
-		// 		how_much = std::abs(min.x() - side_max.x());
-		// 		ROS_INFO_STREAM("Nope, just " << how_much << " = std::abs(" << min.x() << " - " << side_max.x() << ")");
-		// 		return how_much;
-		// 	}
-		// }
-		// else
-		// {
-		// 	return 0;
-		// }
-
 	}
+
+	TEST(VolumeTest, sizeInsideGeofenceMax_half)
+	{
+		
+		double side_x = sizeInsideGeofence_max(0.9, 1.1, 0.2, 1);
+		ASSERT_NEAR(side_x, 0.1, 0.01);
+		
+	}
+	TEST(VolumeTest, sizeInsideGeofenceMax_allIn)
+	{
+		
+		double side_x = sizeInsideGeofence_max(0.9, 1.1, 0.2, 1.1);
+		ASSERT_NEAR(side_x, 0.2, 0.01);
+		
+	}
+	TEST(VolumeTest, sizeInsideGeofenceMax_allOut)
+	{
+		
+		double side_x = sizeInsideGeofence_max(0.9, 1.1, 0.2, 0.9);
+		ASSERT_NEAR(side_x, 0, 0.01);
+		
+	}
+
+
+	TEST(VolumeTest, sizeInsideGeofenceMax_allin)
+	{
+		octomap::OcTree octree ("data/experimentalDataset.bt");
+		octomath::Vector3 min (0, 0, 0);
+		octomath::Vector3 max (1, 1, 1);
+
+		octomap::OcTreeKey bbxMinKey, bbxMaxKey;
+        if(!octree.coordToKeyChecked(min, bbxMinKey) || !octree.coordToKeyChecked(max, bbxMaxKey))
+        {
+            ROS_ERROR_STREAM("[Frontiers] Problems with write_volume_explored_to_csv");
+        }
+		octomap::OcTree::leaf_bbx_iterator it = octree.begin_leafs_bbx(bbxMinKey,bbxMaxKey);
+
+		octomath::Vector3 side_max = it.getCoordinate() + octomath::Vector3(it.getSize()/2, it.getSize()/2, it.getSize()/2); 
+		octomath::Vector3 side_min = it.getCoordinate() - octomath::Vector3(it.getSize()/2, it.getSize()/2, it.getSize()/2); 
+		ROS_INFO_STREAM("Center: " << it.getCoordinate() << " size " << it.getSize() << ". Sides " << side_min << " to " << side_max << ". Geofence " << min << " to " << max);
+		double how_much;
+		bool is_inside = side_max.x() > min.x(); 
+		double side_x = sizeInsideGeofence_max(side_min.x(), side_max.x(), it.getSize(), max.x());
+		ASSERT_EQ(side_x, 0.2);
+	}
+
 
 	TEST(VolumeTest, OutsideOnBorderGeofence)
 	{
