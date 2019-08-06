85a5ca5c10b9c736dde7abc97cfcc3903e08e8a4
Unknown <margaridaCostaFaria@gmail.com>
2019-08-06 13:33:12 +0100

    [architecture] Revert changes in the loop rate.
    With timer it is too slow.

--- architecture/src/gps_utm_tf_broadcaster_node.cpp
+++ architecture/src/gps_utm_tf_broadcaster_node.cpp
@@ -4,12 +4,15 @@
 #include <nav_msgs/Odometry.h>
 #include <gazebo_msgs/GetLinkState.h>
 
-namespace fake_gps
-{
-  ros::Timer timer;
-  ros::ServiceClient get_transform;
+int main(int argc, char** argv){
+  ros::init(argc, argv, "tf_broadcaster");
+  ros::NodeHandle nh;
 
-  void main_loop(const ros::TimerEvent&)
+
+  ros::ServiceClient get_transform           = nh.serviceClient<gazebo_msgs::GetLinkState>("/gazebo/get_link_state");
+  static tf::TransformBroadcaster br;
+
+  while (ros::ok())
   {
     gazebo_msgs::GetLinkState srv;
     srv.request.link_name = "iris_hokuyo_1::base_link";
@@ -22,7 +25,6 @@ namespace fake_gps
       transform.setOrigin( tf::Vector3(srv.response.link_state.pose.position.x, srv.response.link_state.pose.position.y, srv.response.link_state.pose.position.z) );
       tf::Quaternion q (srv.response.link_state.pose.orientation.x, srv.response.link_state.pose.orientation.y, srv.response.link_state.pose.orientation.z, srv.response.link_state.pose.orientation.w);
       transform.setRotation(q);
-      static tf::TransformBroadcaster br;
       br.sendTransform(tf::StampedTransform(transform, timestamp, srv.request.reference_frame, "base_link"));
     }
     else
@@ -30,18 +32,3 @@ namespace fake_gps
         ROS_ERROR("[tf_broadcaster] Gazebo not accepting get_link_state requests.");
     }
   }
-}
-
-int main(int argc, char** argv){
-  ros::init(argc, argv, "tf_broadcaster");
-  ros::NodeHandle nh;
-
-
-  fake_gps::get_transform           = nh.serviceClient<gazebo_msgs::GetLinkState>("/gazebo/get_link_state");
-
-
-  fake_gps::timer = nh.createTimer(ros::Duration(0.3), fake_gps::main_loop);
-  ros::spin();
-
-  return 0;
-};
