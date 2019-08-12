0ea0d25489d4565b8630e1dbad1fa341e54f0006
Unknown <margaridaCostaFaria@gmail.com>
2019-08-12 13:43:29 +0100

    [frontiers] Remove neighbors below for exploration.

--- frontiers/src/neighbors.cpp
+++ frontiers/src/neighbors.cpp
@@ -140,7 +140,7 @@ namespace LazyThetaStarOctree{
                 double n_y = y_start + (j * resolution);
 
                 addIfUnique(neighbors, x_start + (i * resolution),  y_start + (j * resolution),  up_z);
-                addIfUnique(neighbors, x_start + (i * resolution),  y_start + (j * resolution),  down_z);
+                // addIfUnique(neighbors, x_start + (i * resolution),  y_start + (j * resolution),  down_z);
             }
         }
     }
@@ -256,7 +256,10 @@ namespace LazyThetaStarOctree{
                     // it is not, search failed
                     ROS_WARN_STREAM("Failed to find depth ");
                     std::ostringstream oss_filename;
-                    oss_filename << "/ros_ws/src/data/current/failed_to_find_depth__getNodeDepth_Octomap__" 
+
+                    std::stringstream aux_envvar_home (std::getenv("HOME"));
+                    std::string folder_name = aux_envvar_home.str() + "/Flying_Octomap_code/src/data";
+                    oss_filename << folder_name << "/current/failed_to_find_depth__getNodeDepth_Octomap__" 
                         << key[0] << "_" << key[1] << "_" << key[2] << ".bt";
                     octree.writeBinaryConst(oss_filename.str());
 		            std::ostringstream oss;
