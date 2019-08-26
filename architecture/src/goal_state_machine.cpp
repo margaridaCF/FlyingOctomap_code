ceedb23fce2fe7e17feb4a73531453f83f6e8c06
Unknown <margaridaCostaFaria@gmail.com>
2019-08-26 22:16:11 +0100

    [goal_sm] Guarentee the the local fence has minimum width, height and length.

--- architecture/src/goal_state_machine.cpp
+++ architecture/src/goal_state_machine.cpp
@@ -249,6 +249,20 @@ namespace goal_state_machine
 	    pi.marker_pub.publish(marker_array);
 	}
 
+	void constraintToMinimumSizeAndBoundaries(double & candidate_lower_bound, double & candidate_upper_bound, double lower_bound, double upper_bound, double min_length)
+	{
+		if(candidate_lower_bound < lower_bound)
+		{
+			candidate_lower_bound = lower_bound;
+			candidate_upper_bound = std::min(lower_bound + min_length*2, upper_bound);
+		}
+		else if (candidate_upper_bound > upper_bound)
+		{
+			candidate_upper_bound = upper_bound;
+			candidate_lower_bound = std::max(upper_bound - min_length*2, lower_bound);
+		}
+	}
+
 	bool GoalStateMachine::fillLocalGeofence()
 	{
 		Eigen::Vector3d start(success_flyby_start.x, success_flyby_start.y, success_flyby_start.z);
@@ -273,73 +287,15 @@ namespace goal_state_machine
         // Min
         frontier_srv.request.min.x = std::max(std::min({a.x(), b.x(), c.x(), d.x()}), geofence_min.x);
         frontier_srv.request.min.y = std::max(std::min({a.y(), b.y(), c.y(), d.y()}), geofence_min.y);
-        frontier_srv.request.min.z = std::max(std::min({start.z(), end.z()}), geofence_min.z);
+        frontier_srv.request.min.z = start.z()-local_fence_side;
         // Max
         frontier_srv.request.max.x = std::min(std::max({a.x(), b.x(), c.x(), d.x()}), geofence_max.x);
         frontier_srv.request.max.y = std::min(std::max({a.y(), b.y(), c.y(), d.y()}), geofence_max.y);
-        frontier_srv.request.max.z = std::min(frontier_srv.request.min.z+ std::abs(start.z() - end.z()) + range, geofence_max.z);
+        frontier_srv.request.max.z = start.z()+local_fence_side;
         // Guarentee minimum dimensions 
-        frontier_srv.request.min.x = std::min( (frontier_srv.request.min.x ), (frontier_srv.request.max.x - local_fence_side) );
-        frontier_srv.request.min.y = std::min( (frontier_srv.request.min.y ), (frontier_srv.request.max.y - local_fence_side) );
-        frontier_srv.request.min.z = std::min( (frontier_srv.request.min.z ), (frontier_srv.request.max.z - local_fence_side) );
-
-        if(pi.publish)
-        {
-	        visualization_msgs::MarkerArray marker_array;
-	        // octomath::Vector3 a_o(a.x(), a.y(), a.z());
-	        // octomath::Vector3 b_o(b.x(), b.y(), b.z());
-	        // octomath::Vector3 c_o(c.x(), c.y(), c.z());
-	        // octomath::Vector3 d_o(d.x(), d.y(), d.z());
-	        // visualization_msgs::Marker marker;
-	        // octomath::Vector3 start_o(start.x(), start.y(), start.z());
-	        // rviz_interface::build_waypoint(start_o, 0.1, 0,   10, marker, 5);
-	        // marker.ns = "start";
-	        // marker_array.markers.push_back( marker );
-	        // octomath::Vector3 end_o(end.x(), end.y(), end.z());
-	        // rviz_interface::build_waypoint(end_o, 0.1, 0,   11, marker, 5);
-	        // marker.ns = "end";
-	        // marker_array.markers.push_back( marker );
-
-	        // rviz_interface::build_waypoint(a_o, 0.5, 0,   1, marker, 1);
-	        // marker.ns = "a";
-	        // marker_array.markers.push_back( marker );
-	        // rviz_interface::build_waypoint(b_o, 0.5, 250, 2, marker, 1);
-	        // marker.ns = "b";
-	        // marker_array.markers.push_back( marker );
-	        // rviz_interface::build_waypoint(c_o, 0.5, 0  , 3, marker, 9);
-	        // marker.ns = "c";
-	        // marker_array.markers.push_back( marker );
-	        // rviz_interface::build_waypoint(d_o, 0.5, 250, 4, marker, 9);
-	        // marker.ns = "d";
-	        // marker_array.markers.push_back( marker );
-	        // rviz_interface::build_arrow_type(start_o, end_o, marker_array, 20, true);
-	        // octomath::Vector3 end_ortho(start_o.x()+ortho.x(), start_o.y()+ortho.y(), start_o.z()+ortho.z());
-	        // rviz_interface::build_arrow_type(start_o, end_ortho, marker_array, 21, false);
-	        // ROS_INFO_STREAM("");
-	        // ROS_INFO_STREAM("Extension = " << extension );
-	        // ROS_INFO_STREAM("Start = (" << start.x() << ", " << start.y() << ", " << start.z() << ")");
-	        // ROS_INFO_STREAM("End = (" << end.x() << ", " << end.y() << ", " << end.z() << ")");
-	        // ROS_INFO_STREAM("End Ortho = (" << end_ortho.x() << ", " << end_ortho.y() << ", " << end_ortho.z() << ")");
-	        
-	        // ROS_INFO_STREAM("e = (" << e.x() << ", " << e.y() << ", " << e.z() << ")");
-
-	        // ROS_INFO_STREAM("a = (" << a.x() << ", " << a.y() << ", " << a.z() << ")");
-	        // ROS_INFO_STREAM("b = (" << b.x() << ", " << b.y() << ", " << b.z() << ")");
-
-	        // ROS_INFO_STREAM("c = (" << c.x() << ", " << c.y() << ", " << c.z() << ")");
-	        // ROS_INFO_STREAM("d = (" << d.x() << ", " << d.y() << ", " << d.z() << ")");
-
-	        // ROS_INFO_STREAM("Direction = (" << direction.x() << ", " << direction.y() << ", " << direction.z() << ")");
-	        // ROS_INFO_STREAM("Ortho = (" << ortho.x() << ", " << ortho.y() << ", " << ortho.z() << ")");
-	        // ROS_INFO_STREAM("Request " << frontier_srv.request);
-	        octomath::Vector3 min(frontier_srv.request.min.x, frontier_srv.request.min.y, frontier_srv.request.min.z);
-	        octomath::Vector3 max(frontier_srv.request.max.x, frontier_srv.request.max.y, frontier_srv.request.max.z);
-	        // ROS_INFO_STREAM("Min = (" << frontier_srv.request.min.x << ", " << frontier_srv.request.min.y << ", " << frontier_srv.request.min.z << ")");
-	        // ROS_INFO_STREAM("Max = (" << frontier_srv.request.max.x << ", " << frontier_srv.request.max.y << ", " << frontier_srv.request.max.z<< ")");
-	        rviz_interface::publish_geofence (min, max, marker_array);
-
-	        pi.marker_pub.publish(marker_array);
-        }
+        constraintToMinimumSizeAndBoundaries(frontier_srv.request.min.x, frontier_srv.request.max.x, geofence_min.x, geofence_max.x, local_fence_side);
+        constraintToMinimumSizeAndBoundaries(frontier_srv.request.min.y, frontier_srv.request.max.y, geofence_min.y, geofence_max.y, local_fence_side);
+        constraintToMinimumSizeAndBoundaries(frontier_srv.request.min.z, frontier_srv.request.max.z, geofence_min.z, geofence_max.z, local_fence_side);
 	}
 
 	bool GoalStateMachine::findFrontiersAllMap(Eigen::Vector3d& uav_position)
--- frontiers/test/work_in_progress_frontiers_tests.cpp
+++ frontiers/test/work_in_progress_frontiers_tests.cpp
@@ -4,168 +4,80 @@
 
 namespace Frontiers_test
 {
-	void printForMatlab(std::unordered_set<std::shared_ptr<octomath::Vector3>> neighbors)
+	
+	void constraintToMinimumSizeAndBoundaries(double & candidate_lower_bound, double & candidate_upper_bound, double lower_bound, double upper_bound, double min_length)
 	{
-		std::cout << " x_values = [ ];\n";
-		std::cout << " y_values = [ ];\n";
-		std::cout << " z_values = [ ];\n";
-		for (std::shared_ptr<octomath::Vector3> n : neighbors)
+		if(candidate_lower_bound < lower_bound)
 		{
-			std::cout << " x_values = [x_values, " << n->x() << "];\n ";
-			std::cout << " y_values = [y_values, " << n->y() << "];\n ";
-			std::cout << " z_values = [z_values, " << n->z() << "];\n ";
+			candidate_lower_bound = lower_bound;
+			candidate_upper_bound = std::min(lower_bound + min_length, upper_bound);
+		}
+		else if (candidate_upper_bound > upper_bound)
+		{
+			candidate_upper_bound = upper_bound;
+			candidate_lower_bound = std::max(upper_bound - min_length, lower_bound);
 		}
 	}
 
-	double sizeInsideGeofence_min(double side_min, double side_max, double size, double min)
+	TEST(CirculatorTest, constraintToMinimumSizeAndBoundaries)
 	{
-		double how_much;
-		bool is_inside = side_max > min; 
-		// ROS_INFO_STREAM("is_inside: side_max > min <==> " << side_max << " > " << min);
-		if(is_inside)
-		{
-			bool all_in = side_min >= min;
-			// ROS_INFO_STREAM("All in? " << side_min << " > " << min);
-			if (all_in)
-			{
-				// ROS_INFO_STREAM("[Min] Yup, all in!");
-				return size;
-			}
-			else
-			{
-				how_much = std::abs(min - side_max);
-				// ROS_INFO_STREAM("[Min] Nope, just " << how_much << " = std::abs(" << min << " - " << side_max << ")");
-				return how_much;
-			}
-		}
-		else
-		{
-			// ROS_INFO_STREAM("[Min] Completely out!");
-			return 0;
-		}
+		double candidate_lower_bound_solution = 3;
+		double candidate_upper_bound_solution = 10;
+		double candidate_lower_bound = 2;
+		double candidate_upper_bound = 9;
+		double lower_bound = 3;
+		double upper_bound = 10;  
+		double min_length = 7; 
+		constraintToMinimumSizeAndBoundaries(candidate_lower_bound, candidate_upper_bound, lower_bound, upper_bound, min_length);
+		ROS_INFO_STREAM("Result " << candidate_lower_bound << " to " << candidate_upper_bound);
+		ASSERT_EQ(candidate_lower_bound_solution, candidate_lower_bound);
+		ASSERT_EQ(candidate_upper_bound_solution, candidate_upper_bound);
 	}
 
-	double sizeInsideGeofence_max(double side_min, double side_max, double size, double max)
+	TEST(CirculatorTest, constraintToMinimumSizeAndBoundaries_insideGeofence)
 	{
-		double how_much;
-		bool is_inside = side_min < max; 
-		// ROS_INFO_STREAM("is_inside: side_min < max <==> " << side_min << " < " << max);
-		if(is_inside)
-		{
-			bool all_in = side_max <= max;
-			// ROS_INFO_STREAM("All in? " << side_max << " <= " << max);
-			if (all_in)
-			{
-				// ROS_INFO_STREAM("[Max] Yup, all in!");
-				return size;
-			}
-			else
-			{
-				how_much = std::abs(max - side_min);
-				// ROS_INFO_STREAM("[Max] Nope, just " << how_much << " = std::abs(" << max << " - " << side_min<< ")");
-				return how_much;
-			}
-		}
-		else
-		{
-			// ROS_INFO_STREAM("[Max] Completely out!");
-			return 0;
-		}
-	}
-
-	double oneSide(double side_min, double side_max, double size, double min, double max)
-	{
-		double inside_min = sizeInsideGeofence_min(side_min, side_max, size, min);
-		double inside_max = sizeInsideGeofence_max(side_min, side_max, size, max);
-		bool inside = inside_min > 0 && inside_max > 0 ;
-		if(inside)
-		{
-			double side = std::min(inside_min, inside_max);
-			return side;
-		}
-		else
-		{
-			return 0;
-		}
-	}
-
-	double volumeInsideGeofence(octomath::Vector3 const& min, octomath::Vector3 const& max, octomap::OcTree::leaf_bbx_iterator const& it)
-	{
-		octomath::Vector3 side_max = it.getCoordinate() + octomath::Vector3(it.getSize()/2, it.getSize()/2, it.getSize()/2); 
-		octomath::Vector3 side_min = it.getCoordinate() - octomath::Vector3(it.getSize()/2, it.getSize()/2, it.getSize()/2); 
-		ROS_INFO_STREAM("Center: " << it.getCoordinate() << " size " << it.getSize() << ". Sides " << side_min << " to " << side_max << ". Geofence " << min << " to " << max);
-
-		double side_x = oneSide(side_min.x(), side_max.x(), it.getSize(), min.x(), max.x());
-		if(side_x != 0)
-		{
-			double side_y = oneSide(side_min.y(), side_max.y(), it.getSize(), min.y(), max.y());
-			if(side_y != 0)
-			{
-				double side_z = oneSide(side_min.z(), side_max.z(), it.getSize(), min.z(), max.z());
-				double volume = side_x * side_y * side_z;
-				ROS_INFO_STREAM("Volume: " << volume);
-				return volume;
-			}
-		}
-		return 0;	
-	}
-
-	frontiers_msgs::FindFrontiers::Response getFrontiers(octomap::OcTree const& octree, frontiers_msgs::FindFrontiers::Request  &request, ros::Publisher const& marker_pub, Frontiers::search_function search)
-	{
-		octomath::Vector3 min (request.min.x, request.min.y, request.min.z);
-		octomath::Vector3 max (request.max.x, request.max.y, request.max.z);
-		frontiers_msgs::FindFrontiers::Response reply;
-		int start_it = 0;
-        Frontiers::Circulator it (octree, max, min, start_it);
-    	search(octree, it, request, reply, marker_pub, false);
-    	return reply;
-	}
-
-	bool compareVoxelMsg(frontiers_msgs::VoxelMsg & a, frontiers_msgs::VoxelMsg & b)
-	{
-		bool equal = (a.xyz_m.x == b.xyz_m.x);
-		equal = equal && (a.xyz_m.y == b.xyz_m.y);
-		equal = equal && (a.xyz_m.z == b.xyz_m.z);
-		equal = equal && (a.size == b.size);
-		return equal;
-	}
-
-	TEST(CirculatorTest, SearchFrontiers)
-	{
-		ros::Publisher marker_pub;
-		octomap::OcTree octree ("data/hilt_measurements.bt");
-		frontiers_msgs::FindFrontiers::Request  request;
-		request.min.x = -40;
-		request.max.x = 30;
-		
-		request.min.y = -18;
-		request.max.y = 20;
-
-		request.min.z = 4;
-		request.max.z = 35;
-		request.frontier_amount = 1;
-		auto start = std::chrono::high_resolution_clock::now();
-		frontiers_msgs::FindFrontiers::Response reply_v1 = getFrontiers(octree, request, marker_pub, Frontiers::searchFrontier);
-		auto end = std::chrono::high_resolution_clock::now(); 
-		auto time_span = end - start;
-		ROS_INFO_STREAM("v1 took " << std::chrono::duration_cast<std::chrono::microseconds>(time_span).count());
-
-		start = std::chrono::high_resolution_clock::now();
-		frontiers_msgs::FindFrontiers::Response reply_v2 = getFrontiers(octree, request, marker_pub, Frontiers::searchFrontier_optimized);
-		end = std::chrono::high_resolution_clock::now(); 
-		time_span = end - start;
-		ROS_INFO_STREAM("v2 took " << std::chrono::duration_cast<std::chrono::microseconds>(time_span).count());
-
-		ASSERT_EQ(reply_v1.frontiers.size(), reply_v2.frontiers.size());
-		int index = 0;
-		while(index < reply_v1.frontiers.size())
-		{
-			ASSERT_TRUE(   compareVoxelMsg( reply_v1.frontiers[index], reply_v2.frontiers[index] )   );
-			index++;
-		}
+		double candidate_lower_bound_solution = 3;
+		double candidate_upper_bound_solution = 10;
+		double candidate_lower_bound = 2;
+		double candidate_upper_bound = 9;
+		double lower_bound = 3;
+		double upper_bound = 10;  
+		double min_length = 10; 
+		constraintToMinimumSizeAndBoundaries(candidate_lower_bound, candidate_upper_bound, lower_bound, upper_bound, min_length);
+		ROS_INFO_STREAM("Result " << candidate_lower_bound << " to " << candidate_upper_bound);
+		ASSERT_EQ(candidate_lower_bound_solution, candidate_lower_bound);
+		ASSERT_EQ(candidate_upper_bound_solution, candidate_upper_bound);
 	}
 
 
+
+	TEST(CirculatorTest, constraintToMinimumSizeAndBoundaries_upper)
+	{
+		double candidate_lower_bound_solution = 3;
+		double candidate_lower_bound = 5;
+		double candidate_upper_bound = 30;
+		double lower_bound = 3;
+		double upper_bound = 10;  
+		double min_length = 7; 
+		constraintToMinimumSizeAndBoundaries(candidate_lower_bound, candidate_upper_bound, lower_bound, upper_bound, min_length);
+		ROS_INFO_STREAM("Result " << candidate_lower_bound << " to " << candidate_upper_bound);
+		ASSERT_EQ(candidate_lower_bound_solution, candidate_lower_bound);
+		ASSERT_EQ(upper_bound, candidate_upper_bound);
+	}
+
+	TEST(CirculatorTest, constraintToMinimumSizeAndBoundaries_MONKEY)
+	{
+		double candidate_lower_bound_solution = 3;
+		double candidate_lower_bound = -2;
+		double candidate_upper_bound = 12;
+		double lower_bound = 4;
+		double upper_bound = 35;  
+		double min_length = 7; 
+		constraintToMinimumSizeAndBoundaries(candidate_lower_bound, candidate_upper_bound, lower_bound, upper_bound, min_length);
+		ROS_INFO_STREAM("Result " << candidate_lower_bound << " to " << candidate_upper_bound);
+		ASSERT_EQ(candidate_lower_bound_solution, candidate_lower_bound);
+		ASSERT_EQ(upper_bound, candidate_upper_bound);
+	}
 
 }
 
